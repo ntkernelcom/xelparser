c13ed8a1acf560af502dd2bff1ad7fbcfefa8db7
Vadim Smirnov <vadim@ntkernel.com>
2020-03-28 08:34:23 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.XEvent.XELite;
+
+namespace XelReader
+{
+    class Program
+    {
+        internal class XELFileParser
+        {
+            public string InputFilePath { get; }
+
+            public XELFileParser(string filePath)
+            {
+                InputFilePath = filePath;
+            }
+
+            public void Process()
+            {
+
+                if (!File.Exists(InputFilePath))
+                {
+                    return;
+                }
+
+                string inputFileName = Path.GetFileName(InputFilePath);
+                Console.WriteLine($"{inputFileName}");
+                string extension = Path.GetExtension(InputFilePath);
+
+                var xeStream = new XEFileEventStreamer(InputFilePath);
+
+                xeStream.ReadEventStream(
+
+                    xevent =>
+                    {
+                        Console.WriteLine(xevent);
+                        Console.WriteLine("");
+                        return Task.CompletedTask;
+                    },
+                    CancellationToken.None).Wait();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: XelReader file_name");
+                return;
+            }
+            var xel = new XELFileParser(args[0]);
+            xel.Process();
+        }
+    }
+}
